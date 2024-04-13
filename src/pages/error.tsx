bcaa5ba7ec8c36bec866a656fae2affdc8817e9a
Alison Monteiro <alisonmonteiro.10@gmail.com>
2024-04-13 15:40:47 -0300

    improve error page layout

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,3 +1,17 @@
+import { Inter } from "next/font/google";
+import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export default function ErrorPage() {
-  return <p>Sorry, something went wrong</p>
+  return (
+    <main className={`p-4 md:p-10 w-screen h-screen ${inter.className}`}>
+      <Head>
+        <title>Notes - Error</title>
+      </Head>
+      <div className="flex items-center justify-center h-full w-full">
+        <p>Sorry, something went wrong</p>
+      </div>
+    </main>
+  );
 }
